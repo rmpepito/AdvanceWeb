955a62c09d281cb132e6378c9aef0ac873060bad
rmpepito <ronnamaepepito@lsu.edu.ph>
2022-02-19 13:48:57 +0800

    first commit

---
+++ Activity1/src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Welcome to my JSworld</h1>
+<div>
+<hr>
+  <h2> Get to know me </h2>
+  <p>Hello There!
+  <br>My name is Ronna Mae Pepito-Agraviador.
+  <br>I am a mom, a wife, a student, and an entrepreneur.
+  <br>I am already 27 years old and my zodiac is Leo.
+  <br>I'm a 4th year BSIT student.
+  <br>Ask me questions if you want to know more about me :)
+  </p>
+  
+</div>
+`;
